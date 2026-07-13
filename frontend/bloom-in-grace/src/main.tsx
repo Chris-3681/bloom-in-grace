2bdeb5924978aea1a904c465ec0ba0e085f254cb
Crispus Githogori Ng'ang'a <githogoringash91@gmail.com>
2026-07-13 13:19:48 +0300

    Fix PayPal client debug

--- frontend/bloom-in-grace/src/main.tsx
+++ frontend/bloom-in-grace/src/main.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 import "./index.css";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-import { Toaster } from "react-hot-toast"
+
+console.log(
+  "PayPal Client ID:",
+  import.meta.env.VITE_PAYPAL_CLIENT_ID
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  console.log("PayPal Client ID:", import.meta.env.VITE_PAYPAL_CLIENT_ID);
- <React.StrictMode>
-  <BrowserRouter>
-    <PayPalScriptProvider
-      options={{
-        clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
-        currency: "USD",
-        intent: "capture",
-      }}
-    >
-      <App />
-      <Toaster position="top-center" />
-    </PayPalScriptProvider>
-  </BrowserRouter>
-</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <PayPalScriptProvider
+        options={{
+          clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
+          currency: "USD",
+          intent: "capture",
+        }}
+      >
+        <App />
+        <Toaster position="top-center" />
+      </PayPalScriptProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
