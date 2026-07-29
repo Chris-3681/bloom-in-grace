3db31c059ae7dbef4b773b4f02bcfeca928cb4cd
Crispus Githogori Ng'ang'a <githogoringash91@gmail.com>
2026-07-29 19:59:18 +0300

    Remove paypal

--- frontend/bloom-in-grace/src/main.tsx
+++ frontend/bloom-in-grace/src/main.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PayPalScriptProvider
-        options={{
-          clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
-          currency: "USD",
-          intent: "capture",
-        }}
-      >
-        <App />
-        <Toaster position="top-center" />
-      </PayPalScriptProvider>
+      <App />
+      <Toaster position="top-center" />
     </BrowserRouter>
   </React.StrictMode>
 );
