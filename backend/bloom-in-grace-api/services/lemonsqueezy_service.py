264764418c807b3971c98cc4ad58fbdcc1ac09c9
Crispus Githogori Ng'ang'a <githogoringash91@gmail.com>
2026-07-30 14:59:29 +0300

    clear unmatched error

--- backend/bloom-in-grace-api/services/lemonsqueezy_service.py
+++ backend/bloom-in-grace-api/services/lemonsqueezy_service.py
@@ -110,6 +110,9 @@ def create_checkout(product):
 
     )
 
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
     response.raise_for_status()
 
     return response.json()
