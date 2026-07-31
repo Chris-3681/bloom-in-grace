6b8a5b67ea87f113defe281cb7b8b1732d572365
Crispus Githogori Ng'ang'a <githogoringash91@gmail.com>
2026-07-31 14:06:34 +0300

    app.py

--- backend/bloom-in-grace-api/app.py
+++ backend/bloom-in-grace-api/app.py
@@ -96,3 +96,9 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+print("\n========== REGISTERED ROUTES ==========")
+for rule in app.url_map.iter_rules():
+    print(rule)
+print("=======================================\n")
